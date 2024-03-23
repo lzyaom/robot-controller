1eff8de49cacd76eaf54fcce1fd8f11fbdd65ae2
lzy <2397925443@qq.com>
2024-03-23 13:55:35 +0800

    feat: 添加 shadcn-vue

--- packages/visual-programming/vite.config.ts
+++ packages/visual-programming/vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { resolve } from 'path'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import nesting from 'tailwindcss/nesting'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer(), nesting()],
+    },
+  },
   plugins: [
     vueJsx({
       include: [/\.[jt]sx$/],
